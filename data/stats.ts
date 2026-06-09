a5cc5bf7c0fe4f540750f2320c268eb485d67246
Yolène <yoleneconstable@live.fr>
2026-06-09 19:54:53 +0200

    feat: add stats data

---
+++ data/stats.ts
@@ -0,0 +1,20 @@
+import { Statistic } from '../types/statistic';
+
+export const statistics: Statistic[] = [
+  {
+    value: '4+ ans',
+    label: 'avec React & Next.js',
+  },
+  {
+    value: '2 ans',
+    label: "d'alternance en entreprise",
+  },
+  {
+    value: '3',
+    label: 'projets clients en cours',
+  },
+  {
+    value: '100%',
+    label: 'Compatibles mobile, tablette et ordinateur',
+  },
+];
