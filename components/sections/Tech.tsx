3aa523dd5bbc2b7a594176e9a350c80fd06633ed
Yolène <yoleneconstable@live.fr>
2026-06-16 21:06:21 +0200

    style: animate tech section

--- components/sections/Tech.tsx
+++ components/sections/Tech.tsx
@@ -1,5 +1,7 @@
 import Section from '../layout/Section';
 import { technologiesByCategory } from '@/data/technologies';
+import { motion } from 'motion/react';
+import { fadeIn } from '@/lib/animations';
 
 const Tech = () => {
   return (
@@ -10,12 +12,13 @@ const Tech = () => {
             <h3>{category}</h3>
             <div className="grid-layout">
               {technologies.map((tech) => (
-                <span
+                <motion.span
+                  variants={fadeIn}
                   key={tech.name}
                   className="text-2xl md:text-3xl text-muted-foreground"
                 >
                   {tech.name}
-                </span>
+                </motion.span>
               ))}
             </div>
           </div>
